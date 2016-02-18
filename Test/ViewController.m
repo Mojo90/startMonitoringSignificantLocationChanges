a14efb1b4b01f869732e9540441c9d50c5d50863
Moritz Pfeiffer <moritz.pfeiffer.90@gmail.com>
2016-02-18 15:32:13 +0100

    Small fix

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -7,16 +7,36 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
-
+@interface ViewController () <CLLocationManagerDelegate>
+    @property (strong, nonatomic) CLLocationManager *locationManager;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    if(self.locationManager == nil){
+        self.locationManager = [[CLLocationManager alloc] init];
+    }
+    
+    // Set the delegate
+    self.locationManager.delegate = self;
+    [self.locationManager requestAlwaysAuthorization];
+    
+    [self.locationManager startMonitoringSignificantLocationChanges];
+
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+        CLLocation *currentLoc = [locations lastObject];
+    NSLog(@"New Location: %f/%f, Locations: %lu", currentLoc.coordinate.latitude, currentLoc.coordinate.longitude,(unsigned long)[locations count]);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    NSLog(@"Loc Error: %@", [error userInfo]);
 }
 
 - (void)didReceiveMemoryWarning {
